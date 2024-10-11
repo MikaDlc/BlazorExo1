5546488cf078d3cb9c99c73ec64bd683cce41350
Mika Dal Cero <dalcero.mika.1206@outlook.be>
2024-10-11 12:17:37 +0200

    Get all anonyme

---
+++ DemoWASM/Pages/Exercices/Games/Games.razor.cs
@@ -0,0 +1,29 @@
+﻿using DemoWASM.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace DemoWASM.Pages.Exercices.Games
+{
+    public partial class Games
+    {
+        [Inject]
+        public HttpClient Client { get; set; }
+
+        [Inject]
+        public IJSRuntime JS { get; set; }
+
+        public List<Game> Liste { get; set; } = new List<Game>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadData();
+        }
+
+        public async Task LoadData()
+        {
+            Liste = await Client.GetFromJsonAsync<List<Game>>("Game");
+        }
+    }
+}
